61ee15f58f9a6f2299bc280fd2f2abd27d1805ab
Rangel Ivanov <rangel.ivanov@sap.com>
2024-09-14 16:09:33 +0300

    Change Pose detector from RTMO to YOLO-Pose

--- analysis/pose_detection/pose_detector.py
+++ analysis/pose_detection/pose_detector.py
@@ -1,11 +1,10 @@
-import os
-
 import cv2
 import torch
-from rtmlib import RTMO
+from ultralytics import YOLO
 
 from analysis.single_frame_analyzer import SingleFrameAnalyzer
 from analysis.types import AnalysisType
+from util.device import get_device
 
 
 class PoseDetector(SingleFrameAnalyzer):
@@ -16,26 +15,15 @@ class PoseDetector(SingleFrameAnalyzer):
     def analysis_type(self) -> AnalysisType:
         return AnalysisType.PoseEstimation
 
+    @torch.no_grad()
     def analyze(self, frame: cv2.typing.MatLike, *args, **kwargs) -> list[any]:
         if self.model is None:
-            # lazy initialization is done because the RTMO model is not serializable and can't be sent to a child process
-            # these bastards do not support Apple MPS... Only CUDA
-            self.model = RTMO(os.environ["RTMO_MODEL_URL"], device='cuda' if torch.cuda.is_available() else 'cpu')
+            # lazy initialization, due to serialization issues
+            self.model = YOLO('yolov8m-pose.pt')
+            # self.model.to(get_device())
 
-        # top-level dimension (keypoints/scores[N]) - person N
-        # 2nd level (keypoints/scores[N][i]) - keypoints [x, y], scores <float from 0 to 1>
-        keypoints, scores = self.model(frame)
-
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.model.predict(frame, verbose=False, classes=[0])[0]
+        people = results.keypoints.cpu().xy
         # keypoints data format: https://github.com/jin-s13/COCO-WholeBody/blob/master/data_format.md
-
-        # for debugging:
-        # frame = draw_skeleton(frame, keypoints, scores, kpt_thr=0.5)
-
-        # do not filter out low-confidence key points, as confidence will be important for orientation detection
-        result = [
-            # x, y, confidence
-            [(kpt[0], kpt[1], score) for kpt, score in zip(kpts, kscores)]
-            for kpts, kscores in zip(keypoints, scores)
-        ]
-
-        return result
+        return people
