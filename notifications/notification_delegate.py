db02a36de400f5d6822e6a76e037cbe018122f01
Rangel Ivanov <rangel.ivanov@sap.com>
2024-10-27 10:53:21 +0200

    Cleanup notification sender

--- notifications/notification_delegate.py
+++ notifications/notification_delegate.py
@@ -1,7 +1,6 @@
 import logging
 import time
 import requests
-import json
 
 
 def send_notification(address: str) -> None:
@@ -17,18 +16,8 @@ def send_notification(address: str) -> None:
 
 
 def _send_notification_internal(address: str) -> tuple[bool, str]:
-    # should the camera ID be in the URL or the body?
-    payload = {
-        'camera_id': 20
-    }
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    # sanitize the URL, removing any sensitive or unnecessary info
     logging.info(f"Sending notification to: {address}")
-    # strip the path, which contains the camera ID and use it in the message
-    response = requests.post(address, headers=headers, data=json.dumps(payload))
+    response = requests.post(address)
     if response.status_code != 200:
         return False, response.text
     else:
