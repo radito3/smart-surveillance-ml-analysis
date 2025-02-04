3027351b2e88d707ed5c54089cd0a600d0984867
Rangel Ivanov <rangel.ivanov@sap.com>
2025-02-04 16:53:35 +0200

    Make probability sink configurable

--- messaging/sink/probability_result_consumer.py
+++ messaging/sink/probability_result_consumer.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 from messaging.broker_interface import Broker
 from messaging.consumer import Consumer
 from notifications.notification_delegate import send_notification
@@ -8,10 +11,16 @@ class ProbabilityResultConsumer(Consumer):
     def __init__(self, broker: Broker, notification_webhook: str):
         super().__init__(broker, 'classification_results')
         self.notification_webhook_url: str = notification_webhook
+        if 'SINK_PROBABILITY_THRESHOLD' in os.environ:
+            threshold = os.environ['SINK_PROBABILITY_THRESHOLD']
+            if bool(re.match(r'^[01]\.\d*$', threshold)):
+                self.probability_threshold: float = float(threshold)
+        else:
+            self.probability_threshold: float = 0.6  # default threshold
 
     def get_name(self) -> str:
         return 'classification-result-consumer'
 
     def consume_message(self, confidence: float):
-        if confidence > 0.6:  # experiment with threshold values
+        if confidence > self.probability_threshold:  # experiment with threshold values
             send_notification(self.notification_webhook_url)
